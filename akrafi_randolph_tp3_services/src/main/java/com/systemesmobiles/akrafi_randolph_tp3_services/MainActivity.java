fe4bfc882a91ae834561f4e9a892271955443007
Axel RANDOLPH <randolph.axel@yahoo.fr.com>
2020-11-16 16:48:51 +0100

    axel randolph : initialisation module

---
+++ akrafi_randolph_tp3_services/src/main/java/com/systemesmobiles/akrafi_randolph_tp3_services/MainActivity.java
@@ -0,0 +1,14 @@
+package com.systemesmobiles.akrafi_randolph_tp3_services;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ akrafi_randolph_tp3_services/src/test/java/com/systemesmobiles/akrafi_randolph_tp3_services/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.systemesmobiles.akrafi_randolph_tp3_services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
