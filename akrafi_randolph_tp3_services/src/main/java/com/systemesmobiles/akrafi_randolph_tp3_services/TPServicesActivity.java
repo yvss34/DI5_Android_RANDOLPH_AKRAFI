0d47846885d235f0b0251d06c01599c7a89bdc90
Yassine Akrafi <yassine.akrafi@gmail.com>
2020-11-24 17:21:09 +0100

    Yassine AKRAFI : Question 7

--- akrafi_randolph_tp3_services/src/main/java/com/systemesmobiles/akrafi_randolph_tp3_services/TPServicesActivity.java
+++ akrafi_randolph_tp3_services/src/main/java/com/systemesmobiles/akrafi_randolph_tp3_services/TPServicesActivity.java
@@ -8,6 +8,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.systemesmobiles.akrafi_randolph_tp3_services.interfaces.IBackgroundServiceListener;
+
+import java.util.Date;
+
 public class TPServicesActivity extends AppCompatActivity {
 
     @Override
@@ -23,6 +27,7 @@ public class TPServicesActivity extends AppCompatActivity {
         Button buttonStop = (Button) findViewById(R.id. buttonStop);
 
         Intent intent = new Intent(this, BackgroundService.class);
+        TPServicesActivity activity = this;
 
         //action sur le boutton Start
         buttonStart.setOnClickListener(new Button.OnClickListener() {
@@ -32,7 +37,19 @@ public class TPServicesActivity extends AppCompatActivity {
         //action sur le boutton Connexion
         buttonConnexion.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
-
+                Intent intent = new Intent(activity,BackgroundService.class);
+                //Création des listeners
+                IBackgroundServiceListener listener = new IBackgroundServiceListener() {
+                    public void dataChanged(final Object data) {
+                        this.runOnUiThread(new Runnable() {
+                            public void run() {
+                                // Mise à jour de l'UI
+                                final Date date = (Date) data;
+                                champDeTexte.setText((String.valueOf(date.getHours()) + ":" + String.valueOf(date.getMinutes()) + ":" + String.valueOf(date.getSeconds())));
+                            }
+                        });
+                    }
+                };
             }
         });
 
