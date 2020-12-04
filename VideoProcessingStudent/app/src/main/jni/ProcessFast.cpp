5b975e1db973414c2a5c72e34e49030ebaa05069
Axel RANDOLPH <randolph.axel@yahoo.fr.com>
2020-12-04 01:04:12 +0100

    axel randolph : sobel cpp

--- VideoProcessingStudent/app/src/main/jni/ProcessFast.cpp
+++ VideoProcessingStudent/app/src/main/jni/ProcessFast.cpp
@@ -7,6 +7,10 @@
 #include <jni.h>
 
 using namespace std;
+
+// GRADIENT
+
+/*
 extern "C" {
 JNIEXPORT void JNICALL Java_com_example_myapplicationopencv_MainActivity_ProcessFast(JNIEnv* env, jobject thiz, jint width,jint height, jbyteArray data, jbyteArray out)
     {
@@ -50,4 +54,53 @@ JNIEXPORT void JNICALL Java_com_example_myapplicationopencv_MainActivity_Process
     env->ReleaseByteArrayElements(data, _data, 0);
     env->ReleaseByteArrayElements(out, _out, 0);
     }
+}
+
+*/
+
+// SOBEL
+
+extern "C" {
+JNIEXPORT void JNICALL Java_com_example_myapplicationopencv_MainActivity_processFast(JNIEnv* env, jobject thiz, jint width,jint height, jbyteArray data, jbyteArray out)
+{
+jbyte* _data = env->GetByteArrayElements(data, 0);
+jbyte* _out = env->GetByteArrayElements(out, 0);
+//stuf todo here
+
+int sobel_x[3][3] = {{-1,0,1},{-2,0,2},{-1,0,1}};
+int sobel_y[3][3] = {{-1,-2,-1},{0,0,0},{1,2,1}};
+
+int pixel_x;
+int pixel_y;
+
+for(int x=1;x<width-2;x++) {
+for (int y = 1; y < height - 2; y++) {
+pixel_x = (sobel_x[0][0] * _data[(y-1)*width+(x-1)]) + (sobel_x[0][1] * _data[(y-1)*width+(x)]) + (sobel_x[0][2] * _data[(y+1)*width+(x+1)]) +
+          (sobel_x[1][0] * _data[(y)*width+(x-1)])   + (sobel_x[1][1] * _data[(y)*width+(x)])   + (sobel_x[1][2] * _data[(y)*width+(x+1)]) +
+          (sobel_x[2][0] * _data[(y+1)*width+(x-1)]) + (sobel_x[2][1] * _data[(y+1)*width+(x)]) + (sobel_x[2][2] * _data[(y+1)*width+(x+1)]);
+pixel_y = (sobel_y[0][0] * _data[(y-1)*width+(x-1)]) + (sobel_y[0][1] * _data[(y-1)*width+(x)]) + (sobel_y[0][2] * _data[(y+1)*width+(x+1)]) +
+          (sobel_y[1][0] * _data[(y)*width+(x-1)])   + (sobel_y[1][1] * _data[(y)*width+(x)])   + (sobel_y[1][2] * _data[(y)*width+(x+1)]) +
+          (sobel_y[2][0] * _data[(y+1)*width+(x-1)]) + (sobel_y[2][1] * _data[(y+1)*width+(x)]) + (sobel_x[2][2] * _data[(y+1)*width+(x+1)]);
+
+int val = (int)sqrt((pixel_x * pixel_x) + (pixel_y * pixel_y));
+
+if(val < 0)
+{
+val = 0;
+}
+
+if(val > 255)
+{
+val = 255;
+}
+
+_out[y*width+x] = (jbyte)val;
+}
+
+}
+
+env->ReleaseByteArrayElements(data, _data, 0);
+env->ReleaseByteArrayElements(out, _out, 0);
+
+}
 }
