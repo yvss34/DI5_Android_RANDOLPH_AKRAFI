0707388545a589a4143e7b40a8cdbcb1ae286f78
Yassine Akrafi <yassine.akrafi@gmail.com>
2020-12-08 11:45:52 +0100

    Axel + Yassine : Merge

--- tp5/src/main/java/com/example/tp5/MainActivity.java
+++ tp5/src/main/java/com/example/tp5/MainActivity.java
@@ -27,46 +27,49 @@ public class MainActivity extends AppCompatActivity {
         Button btaction = (Button) findViewById(R.id. button);
         TextView texte = (TextView) findViewById(R.id.textView);
 
-        //mise en place de l'url
-        try {
-            URL newurl = new URL("http://www.google.com");
-            //apppel à CallWebAPI
-            CallWebAPI c = new CallWebAPI(texte);
-            c.execute(newurl.toString());
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-
-        //action sur le bouton
         btaction.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
+                                        public void onClick(View v) {
+                                            //mise en place de l'url
+                                            try {
+                                                URL newurl = new URL("http://www.google.com");
+                                                //apppel à CallWebAPI
+                                                CallWebAPI c = new CallWebAPI(texte);
+                                                c.execute(newurl.toString());
+                                            } catch (MalformedURLException e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                    });
+//        //action sur le bouton
+//        btaction.setOnClickListener(new Button.OnClickListener() {
+//            public void onClick(View v) {
+//
+//                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+//                StrictMode.setThreadPolicy(policy);
+//                HttpURLConnection urlConnection =null;
+//                try {
+//                    URL url = new URL("http://www.google.com/");
+//                    urlConnection = (HttpURLConnection) url.openConnection();
+//                    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+//                    readStream(in);
+//                    urlConnection.disconnect();
+//                } catch (Exception e) {
+//    // TODO Auto-generated catch block
+//                    e.printStackTrace();
+//                    urlConnection.disconnect();
+//                }
+//            }
 
-                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-                StrictMode.setThreadPolicy(policy);
-                HttpURLConnection urlConnection =null;
-                try {
-                    URL url = new URL("http://www.google.com/");
-                    urlConnection = (HttpURLConnection) url.openConnection();
-                    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                    readStream(in);
-                    urlConnection.disconnect();
-                } catch (Exception e) {
-// TODO Auto-generated catch block
-                    e.printStackTrace();
-                    urlConnection.disconnect();
-                }
-            }
+//            private void readStream(InputStream in) {
+//                String text = null;
+//                try (Scanner scanner = new Scanner(in, Charset.forName("UTF-8").name())) {
+//                    text = scanner.useDelimiter("\\A").next();
+//                }
+//
+//                texte.setText(text);
+//            }
 
-            private void readStream(InputStream in) {
-                String text = null;
-                try (Scanner scanner = new Scanner(in, Charset.forName("UTF-8").name())) {
-                    text = scanner.useDelimiter("\\A").next();
-                }
-
-                texte.setText(text);
-            }
-
-        });
+//        });
 
 
     }
